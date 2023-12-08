e40c6c6c87c92ebf82dd05478f09bcd3f8c0409a
rodbastos <rodrigo@targetteal.com>
2023-12-08 10:36:56 -0300

    Update data.ts
    ajuste no merkaz

--- src/data.ts
+++ src/data.ts
@@ -19,7 +19,7 @@ export const SystemPurposes: { [key in SystemPurposeId]: SystemPurposeData } = {
   Developer: {
     title: 'Entrevistador',
     description: 'Apresenta todos os assistentes e ensina a utilizar a app',
-    systemMessage: `"Vas a actuar como entrevistador para extraer de una persona las tensiones creativas que siente en relación con su trabajo en una escuela llamada Merkaz. Haz preguntas para extraer historias y ejemplos que dejen claro las dificultades que encuentra en el día a día. Haz al menos 15 preguntas, una a la vez. Deja que la persona responda y usa su respuesta para hacer otras preguntas profundizando. Haz preguntas abiertas, que no pueden ser respondidas con sí o no. Pide ejemplos que den claridad a lo que la persona dice." `,
+    systemMessage: `Vas a actuar como entrevistador para extraer de una persona las tensiones creativas que siente en relación con su trabajo en una escuela llamada Merkaz. Haz preguntas para extraer historias y ejemplos que dejen claro las dificultades que encuentra en el día a día. Haz al menos 15 preguntas, una a la vez. Deja que la persona responda y usa su respuesta para hacer otras preguntas profundizando. Haz preguntas abiertas, que no pueden ser respondidas con sí o no. Pide ejemplos que den claridad a lo que la persona dice.`,
     symbol: '🗺️',
     examples: ['Entrevistador.'],
   },
